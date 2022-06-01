3631d4d26cea064ac955abafdadbe42417085304
liplum <Li_plum@outlook.com>
2022-06-01 09:06:06 +0800

    Updated maven-publish

--- path/build.gradle.kts
+++ path/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     java
+    `maven-publish`
 }
+version = "1.0"
+group = "plumy.path"
 sourceSets {
     main {
         java.srcDir("src")
@@ -21,5 +24,16 @@ tasks.withType<Test>().configureEach {
     testLogging {
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
         showStandardStreams = true
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "net.liplum"
+            artifactId = "library"
+            version = "1.0"
+
+            from(components["java"])
+        }
     }
 }
