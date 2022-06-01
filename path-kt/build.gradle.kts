bbdcc47f93c4fb0615a8ec2e7da0a9e036ba84ab
liplum <Li_plum@outlook.com>
2022-06-01 13:26:11 +0800

    Published sources-jar that's useful for Kotlin.

--- path-kt/build.gradle.kts
+++ path-kt/build.gradle.kts
@@ -14,13 +14,30 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform {
+tasks {
+    val sourcesJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
     }
-    testLogging {
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-        showStandardStreams = true
+ /*   val javadocJar by creating(Jar::class) {
+        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
+        archiveClassifier.set("javadoc")
+        from(javadoc)
+    }*/
+
+    artifacts {
+        add("archives", sourcesJar)
+        //add("archives", javadocJar)
+    }
+
+    withType<Test>().configureEach {
+        useJUnitPlatform {
+        }
+        testLogging {
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            showStandardStreams = true
+        }
     }
 }
 publishing {
